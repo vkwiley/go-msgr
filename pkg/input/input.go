77fac2e9b2d514b4f467fe80deb2b673d1c05612
Vadim Kilimnichenko <vkilimnich@wiley.com>
2021-09-18 21:43:21 +0300

    Hackaton: [msgr] Add input module

---
+++ pkg/input/input.go
@@ -0,0 +1,35 @@
+package input
+
+import (
+	"go-msgr/pkg/transport"
+	"go-msgr/pkg/display"
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Input struct {
+	sender transport.Sender
+	disp *display.Display
+	destination string
+}
+
+func New(sender transport.Sender, disp *display.Display, destination string) Input {
+	return Input{sender, disp, destination}
+}
+
+func (i *Input) Run() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			i.disp.Show(fmt.Errorf("Input read string error: %w", err).Error())
+			continue
+		}
+		err = i.sender.Send(i.destination, text)
+		if err != nil {
+			i.disp.Show(fmt.Errorf("Input send error: %w", err).Error())
+			continue
+		}
+	}
+}
